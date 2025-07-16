a4ad591cc54cd663a933478492f8b074fb680417
Andriï Zakhartchenko <andriizakhartchenko@outlook.fr>
2025-07-16 20:48:41 +0200

    save

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,14 @@
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery'); 
 const largeImg = document.querySelector('#largeImg');
 
-largeImg.setAttribute('src', '/images/landscape-first.png');
+largeImg.setAttribute('src', '/images/landscape-first.png'); 
 
-gallery.addEventListener('click', function (e) {
-  const link = e.target.closest('.list-item__link');
-
-  if (link) {
-    e.preventDefault();
-
-    const linkHref = link.getAttribute('href');
-
-    if (!linkHref.startsWith('http')) {
-      const baseUrl = window.location.origin;
-
-      largeImg.setAttribute('src', `${baseUrl}${linkHref}`);
-    } else {
-      largeImg.setAttribute('src', linkHref);
+gallery.addEventListener('click', function(e) {
+    const link = e.target.closest('.list-item__link');
+    if (link) {
+        e.preventDefault();
+        const linkHref = link.getAttribute('href');
+        
+        largeImg.setAttribute('src', linkHref); 
     }
-  }
 });
